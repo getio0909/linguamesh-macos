2ea56c2d2aab3adde964b77256518c4769e63e7d
wangtinghu <1992919981@qq.com>
2026-07-28 12:16:07 -0700

    ci: pin current project specification

--- tools/check-source.sh
+++ tools/check-source.sh
@@ -31,7 +31,7 @@ for file in "${required_files[@]}"; do
 done
 
 expected_goal='11f9a65927aac7e57e2af119e9d21cc98e8d5a08b8a112a19ee1c47903e36198'
-expected_project_revision='b75d4d1df2adbb3729db9425f7b999f62673e22c'
+expected_project_revision='cf0aa0adff35a188603587592edb5877b6e26d67'
 expected_core_revision='48a2d59e19611400499954384250f522e0fa6561'
 git -C ../linguamesh-project merge-base --is-ancestor "$expected_project_revision" HEAD || {
     printf '%s\n' 'Project checkout does not contain the compatibility-pinned revision.' >&2
